9c2907282664e8cc2c256730d5271e1c00556757
Reem-Elfalah <r.fellah@yahoo.com>
2020-04-22 01:02:53 +0000

    fimal

--- Gantt.js
+++ Gantt.js
@@ -1,4 +1,4 @@
-function getDaysInMonth(month, year) {
+function getDaysInMonth(month, year) { //get names of months 
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   month -= 1;
   var m = months[month];
@@ -8,94 +8,98 @@ function getDaysInMonth(month, year) {
     days.push(new Date(date).getDate() + ' ' + m);
     date.setDate(date.getDate() +1);
   }
-  return days;
+  return days; //return an array with all days and month name [1 Apr , 2 Apr.....]
 }
-$('#subMonth').on('click', function() {
-	$('.vertical').html('');
-	var month_year = $('#month').val();
-	var myArr = month_year.split('-');
-	myArr = myArr.map(function(el){
-	return parseInt(el)});
-	var year = myArr[0];
-	var month = myArr[1];
-	var daysElement = getDaysInMonth(month, year);
-	for(var i = 0; i < daysElement.length; i++) {
-		var newli = $('<li>' + daysElement[i] +'</li>')
-		var day = daysElement[i].split(" ");
-		day = day[0];
-		if(day.length === 1) 
-		day = '0' + day;
-		var attr = month_year + '-' + day;
-		newli.attr('day', attr);
-		newli.appendTo('.vertical');
-	}
-	var minDay = month_year + '-0' + daysElement[0].split(" ")[0];
-	var maxDay = month_year + '-' + daysElement[daysElement.length - 1].split(" ")[0];
-	$('#from').attr('min', minDay);
-	$('#from').attr('max', maxDay);
-	$('#to').attr('max', maxDay);
-})
 
-$("#from").change(function() {
-	var min = $("#from").val();
-    $('#to').attr('min', min);
-})
-function limitText(field, maxChar){
+function limitText(field, maxChar) {	//limit field characters
 	$(field).attr('maxlength',maxChar);
 }
-$("#to").change(function(){
-	if($("#from").val() == $('#to').val())
-		limitText('#tlabel', 4)
-	else
-		$('#tlabel').attr('maxlength', 15)
+
+
+//-----------------------------------------------------------------------------------------------
+$('#subMonth').on('click', function() {
+	$('.vertical').html(''); //clear html
+	$('.horizontal-task').html(''); //clear html;
+	var month_year = $('#month').val(); //get the month value
+	var myArr = month_year.split('-');	//split year and month
+	myArr = myArr.map(function(el){		//map each element from string to number
+	return parseInt(el)});
+	var year = myArr[0]; 				//get year
+	var month = myArr[1];				//get Month
+	var daysElement = getDaysInMonth(month, year); //get days & month array
+	for(var i = 0; i < daysElement.length; i++) {	//loop in the array and create li elements
+		var newli = $('<li>' + daysElement[i] +'</li>')	//create li element with text of element
+		var day = daysElement[i].split(" ");			//split to get the day
+		day = day[0];				
+		if(day.length === 1)							//if length is 1 add 0 so it's 01
+		day = '0' + day;
+		var attr = month_year + '-' + day;				//create attr called day with day-month-year
+		newli.attr('day', attr);						//each li will have unique attr val
+		newli.appendTo('.vertical');					//append it to vertical container	
+	}
+	var minDay = month_year + '-0' + daysElement[0].split(" ")[0];	//minDay first day in a month
+	var maxDay = month_year + '-' + daysElement[daysElement.length - 1].split(" ")[0]; //Last day
+	$('#from').attr('min', minDay);	//adjust from datepicket min attr to minDay
+	$('#from').attr('max', maxDay);	//adjust from datepicket max attr to maxDay
+	$('#to').attr('max', maxDay);	//adjust to datepicket max attr to maxDay so it's only within this month
+	$('.content .vertical').css({'border-radius': '14px', 'border-bottom-style': 'solid', 'border-bottom-color': 'grey', 'border-top-style': 'solid', 'border-top-color': '#C6C3C1'}) //style the border
 })
 
 
+$("#from").change(function() { //if from is selected adjust to so it's after it
+	var min = $("#from").val();	//min attr of To will start from min of from
+    $('#to').attr('min', min);
+})
 
-$("#task").on("click", function() {
-	if($("#tlabel").val() == '')
+
+$("#to").change(function(){
+	if($("#from").val() == $('#to').val())	//only one day possibility was taken ito account!
+		limitText('#tlabel', 4)
+	else
+		$('#tlabel').attr('maxlength', 20)	//however taskname shouldn't exceed 20characters
+})
+
+
+$("#task").on("click", function() {//submit task
+	if($("#tlabel").val() == '')	//check for empty values
 		return alert('Enter a Task!')
 	else if( $("#from").val() == '' || $("#to").val() == '')
 		return alert('Enter dates!')
 	else if(parseInt($("#to").val().split('-')[2]) - parseInt($("#from").val().split('-')[2]) < 0)
-		return alert('Wrong Dates!')
-	const days = document.querySelectorAll(".content .vertical li");
-	const daysArray = [...days];
-	const taskSubmit = document.querySelectorAll("#tlabel");
-	var color = $("#color").val();
-	var taskName = $("#tlabel").val();
-	var from =	$("#from").val();
-	var to = $("#to").val();
-	var attr = from + taskName.split(" ").join("") + to 
-	var newul = $('<div><ul><li>' + taskName + '</li></ul></div>');
-	newul.attr('duration', attr);
+		return alert('Wrong Dates!')	//Or wrong dates
+	const days = document.querySelectorAll(".content .vertical li");//get all month days in array
+	const daysArray = [...days];	//create a clone
+	const fromlabel = document.querySelectorAll("#from");//select tlabel to ajust the placement of tasks according to it
+	var color = $("#color").val(); //get color value "team name"
+	var taskName = $("#tlabel").val();	//taskName
+	var from =	$("#from").val();	//from date
+	var to = $("#to").val();	//to date
+	var attr = from + taskName.split(" ").join("") + to //create unique attr for each task
+	var newul = $('<div><ul><li>' + taskName + '</li></ul></div>');	//div container, ul to control progress, li to hold text
+	newul.attr('duration', attr);//add attr;
 	$(newul).appendTo('.horizontal-task');
-	var selectAttrdiv = "div[duration = " + attr + "]";
+	var filteredArray = daysArray.filter(el => el.attributes.day.value == from);//filter array using from
+	var left = filteredArray[0].offsetLeft - 8; //left placement of task "8 for adjustment"
+ 	var filteredArray = daysArray.filter(el => el.attributes.day.value == to);	//filter array using to
+ 	var width = filteredArray[0].offsetLeft + filteredArray[0].offsetWidth - left - 10;	//calulate width of task
+ 	var bottom = fromlabel[0].offsetTop + 670;	//scaling with help of from label	
+ 	var selectAttrdiv = "div[duration = " + attr + "]";	//Define selectors
 	var selectAttrul = "div[duration = " + attr + "] ul";
 	var selectAttrli = "div[duration = " + attr + "] ul li";
-	$(selectAttrli).attr('duration', attr);
-	$(selectAttrul).attr('duration', attr);
-	var filteredArray = daysArray.filter(el => el.attributes.day.value == from);
-	var left = filteredArray[0].offsetLeft - 8;
- 	var filteredArray = daysArray.filter(el => el.attributes.day.value == to);
- 	var width = filteredArray[0].offsetLeft + filteredArray[0].offsetWidth - left - 10;
- 	var bottom = 1000 - taskSubmit[0].offsetTop;
- 	var wli = width * 0.5 
-	$(selectAttrdiv).css({'left': left,'width': width, 'bottom': bottom, 'background-color': color, 'filter': 'brightness(120%)','padding': '0px', 'border-radius': '10px'})
-	$(selectAttrul).css ({ 'width': '0px', 'padding': '10px 0px 10px 0','background-color':color, 'filter': 'brightness(80%)', 'border-radius': '10px', 'text-align': 'center'})
-	$(selectAttrli).css({'text-align': 'center', 'display': 'inline-block', 'width': width})
-})
+	$(selectAttrdiv).css({'left': left,'width': width, 'bottom': bottom, 'background-color': color, 'filter': 'brightness(130%)','padding': '0px', 'border-radius': '10px'}) //div css
+	$(selectAttrul).css ({ 'width': '0px', 'padding': '10px 0px 10px 0','background-color':color, 'filter': 'brightness(80%)', 'border-radius': '10px', 'text-align': 'center'}) //ul css
+	$(selectAttrli).css({'color':'black','text-align': 'center', 'display': 'inline-block', 'width': width}) //li css
+});
 
-$('.horizontal-task').on('click', function(){
-	var x = event.target.parentNode
-	var percent;
-	percent = prompt("Please enter prgress", "0%");
-	if(!(parseInt(percent.split('%')[0]) <= 100 && parseInt(percent.split('%')[0]) > 0)) {
-		while(!(parseInt(percent.split('%')[0]) <= 100 && parseInt(percent.split('%')[0]) > 0)){
+$('.horizontal-task').on('click', function(){//Adding progress percentage
+	var x = event.target.parentNode; //get parent node of clicked item
+	var percent = prompt("Please enter prgress", "0%"); //promote enter percentage
+	if(!(parseInt(percent.split('%')[0]) <= 100 && parseInt(percent.split('%')[0]) > 0)) {//if it's not a valid input
+		while(!(parseInt(percent.split('%')[0]) <= 100 && parseInt(percent.split('%')[0]) > 0)){ //use while loop to promote input again
 			percent = prompt("Please enter prgress", "0%");
-				if(parseInt(percent.split('%')[0]) <= 100 && parseInt(percent.split('%')[0]) > 0)
-					break;
+				if(parseInt(percent.split('%')[0]) <= 100 && parseInt(percent.split('%')[0]) > 0)//if it's valid
+					break;	//break the loop
 		}
 	}
-	$(x).css('width', percent)
+	$(x).css('width', percent)//modify the parent element (ul) width with percentage
 })
